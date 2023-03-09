b6ef70fe4cfa3679976e3b6695bbf7d5c5eeb537
Gregory Trabajo <glrd4726ml@gmail.com>
2023-03-09 09:39:19 -0300

    Entrenando bot y creando flujos

--- app.js
+++ app.js
@@ -4,20 +4,26 @@ const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-const flowPrincipal = addKeyword(['Hola','ola','hoa','Hey','Buenas','Buenos','Buenos dias','Buenas tardes','Buenas noches'])
-    .addAnswer('🙌 Hola bienvenido a este *Chatbot*, en breves momentos estaremos constestando todas tus dudas')
-    .addAnswer(
-        [
-            '',
-        ],
-        null,
-        null,
-        []
-    )
+const flowPrincipal = addKeyword(['Hola','ola','hoa','Hey','Buenas','Buenos','Buenos dias','Buenas tardes','Buenas noches','web','suple','websup','websuple','websuplementos','suplementos','suplementos.com','vitatech','ultratech'])
+    .addAnswer(['🙌 Hola bienvenido al *Chatbot* de Suplementos.com'])
+    
+const flowTerminos = addKeyword(['Terminos'])
+    .addAnswer(['Bienvenido a los terminos y condiciones de uso de nuestro ChatBot'])
+    
+const flowListaProd = addKeyword(['Lista', 'Productos'])
+    .addAnswer(['En breves momentos le enviaremos la lista de productos con precios actualizados'])
+
+const flowPedido = addKeyword(['Pedido','Compra'])
+    .addAnswer(['A continuación, se van a solicitar una serie de datos para concretar su Pedido / Compra'])
+    .addAnswer(['Por favor, Indique su nombre'])
+    .addAnswer(['Por favor, indique su número de documento'])
+    .addAnswer(['Por favor, indique su numero telefonico'])
+    .addAnswer(['Por favor, indique su email'])
+    
 
 const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal])
+    const adapterFlow = createFlow([flowPrincipal, flowTerminos, flowListaProd, flowPedido])
     const adapterProvider = createProvider(BaileysProvider)
 
     createBot({
